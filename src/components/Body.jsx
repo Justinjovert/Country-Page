7a6e4e63886f16c3a674683c866b48926f14766c
Justinjovert <justinjoags@gmail.com>
2025-06-03 15:50:33 +0800

    fixed base config

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -11,7 +11,7 @@ function Body() {
         <section className='flex flex-1 justify-center box-border relative bg-[#F5F5F5] dark:bg-[#1c1d1f] text-black dark:text-[#F5F5F5] pb-4 '>
             <div className='h-full w-full relative flex justify-center'>
                 {
-                    <BrowserRouter basename='/Country-page'>
+                    <BrowserRouter basename='/country-page'>
                         <Routes>
                             <Route path='/' element={<HomePage />} />
                             <Route path='/:country' element={<CountryDetail />} />
